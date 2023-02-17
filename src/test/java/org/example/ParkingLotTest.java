a0976ce93755cb89d471b11737801a50b787a920
Amit Bhagat <amitb@sahaj.ai>
2023-02-17 09:54:10 +0530

    Modify tests for car

--- src/test/java/org/example/ParkingLotTest.java
+++ src/test/java/org/example/ParkingLotTest.java
@@ -18,63 +18,6 @@ class ParkingLotTest {
     }
 
     @Test
-    void testMotorcycleParkingAtMall(){
-        Map<VehicleType, List<ParkingSpot>> spots= new HashMap<>();
-        spots.put(VehicleType.MOTORCYCLE, List.of(new ParkingSpot()));
-        ParkingFloor parkingFloor = new ParkingFloor(spots);
-        List<ParkingFloor> floors = new ArrayList<>();
-        floors.add(parkingFloor);
-        Building building = new Building(floors, BuildingType.MALL);
-        List<Building> buildings = new ArrayList<>();
-        buildings.add(building);
-        ParkingLot parkingLot = new ParkingLot(buildings);
-
-        Vehicle vehicle = new Vehicle(1, VehicleType.MOTORCYCLE);
-
-        ParkingSpot parkingSpot = parkingLot.parkVehicle(vehicle);
-
-        assertNotNull(parkingSpot);
-    }
-
-    @Test
-    void testMotorcycleParkingAtStadium(){
-        Map<VehicleType, List<ParkingSpot>> spots= new HashMap<>();
-        spots.put(VehicleType.MOTORCYCLE, List.of(new ParkingSpot()));
-        ParkingFloor parkingFloor = new ParkingFloor(spots);
-        List<ParkingFloor> floors = new ArrayList<>();
-        floors.add(parkingFloor);
-        Building building = new Building(floors, BuildingType.STADIUM);
-        List<Building> buildings = new ArrayList<>();
-        buildings.add(building);
-        ParkingLot parkingLot = new ParkingLot(buildings);
-
-        Vehicle vehicle = new Vehicle(1, VehicleType.MOTORCYCLE);
-
-        ParkingSpot parkingSpot = parkingLot.parkVehicle(vehicle);
-
-        assertNotNull(parkingSpot);
-    }
-
-    @Test
-    void testMotorcycleParkingAtAirport(){
-        Map<VehicleType, List<ParkingSpot>> spots= new HashMap<>();
-        spots.put(VehicleType.MOTORCYCLE, List.of(new ParkingSpot()));
-        ParkingFloor parkingFloor = new ParkingFloor(spots);
-        List<ParkingFloor> floors = new ArrayList<>();
-        floors.add(parkingFloor);
-        Building building = new Building(floors, BuildingType.AIRPORT);
-        List<Building> buildings = new ArrayList<>();
-        buildings.add(building);
-        ParkingLot parkingLot = new ParkingLot(buildings);
-
-        Vehicle vehicle = new Vehicle(1, VehicleType.MOTORCYCLE);
-
-        ParkingSpot parkingSpot = parkingLot.parkVehicle(vehicle);
-
-        assertNotNull(parkingSpot);
-    }
-
-    @Test
     void testCarParkingAtMall(){
         Map<VehicleType, List<ParkingSpot>> spots= new HashMap<>();
         spots.put(VehicleType.CAR, List.of(new ParkingSpot()));
@@ -87,25 +30,6 @@ class ParkingLotTest {
         ParkingLot parkingLot = new ParkingLot(buildings);
 
         Vehicle vehicle = new Vehicle(1, VehicleType.CAR);
-
-        ParkingSpot parkingSpot = parkingLot.parkVehicle(vehicle);
-
-        assertNotNull(parkingSpot);
-    }
-
-    @Test
-    void testBusParkingAtMall(){
-        Map<VehicleType, List<ParkingSpot>> spots= new HashMap<>();
-        spots.put(VehicleType.BUS, List.of(new ParkingSpot()));
-        ParkingFloor parkingFloor = new ParkingFloor(spots);
-        List<ParkingFloor> floors = new ArrayList<>();
-        floors.add(parkingFloor);
-        Building building = new Building(floors, BuildingType.MALL);
-        List<Building> buildings = new ArrayList<>();
-        buildings.add(building);
-        ParkingLot parkingLot = new ParkingLot(buildings);
-
-        Vehicle vehicle = new Vehicle(1, VehicleType.BUS);
 
         ParkingSpot parkingSpot = parkingLot.parkVehicle(vehicle);
 
